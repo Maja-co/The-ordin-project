ac8536dd2949b6a32f47e9b45276afe284a40ad7
Maja-co <onlineshit61@gmail.com>
2025-11-21 09:46:39 +0100

    Basic basicFunction

---
+++ Beginning/Javascript/basic.js
@@ -0,0 +1,27 @@
+// Opgave 1
+console.log(23+97)
+
+// Opgave 3
+console.log((4+6+9)/7)
+
+
+// Opgave 4
+let a = 10
+console.log(a)
+a = 8
+console.log(a)
+
+// Opgave 4.4
+a = 10
+
+let b = 7* a
+console.log(b)
+
+const max = 57
+
+const actual = max - 13
+
+const percentage = actual / max
+
+console.log(percentage)
+
